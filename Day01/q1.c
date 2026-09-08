588a0b1b65784008eb27e83ae002cf71d37b4ece
Naman Vikram Singh <Naman.42641@stu.upes.ac.in>
2026-09-08 18:58:01 +0530

    Day 01

---
+++ Day01/q1.c
@@ -0,0 +1,18 @@
+/*
+* Name : Naman Vikram Singh
+* Roll : 590042641
+* Day : 05 Question: 1
+* Date : 8-09-2026
+*
+* PROBLEM STATEMENT: 
+* <Q1: Write a program to input two numbers and display their sum>
+*/
+#include<stdio.h>
+int main() {
+    int a, b, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum = %d", sum);
+    return 0;
+}
