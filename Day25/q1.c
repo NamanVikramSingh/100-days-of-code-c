d77451660e859c97226e631e0f0eb9701d229527
Naman Vikram Singh <Naman.42641@stu.upes.ac.in>
2026-09-10 10:29:33 +0530

    Day 25

---
+++ Day25/q1.c
@@ -0,0 +1,27 @@
+/*
+* Name : Naman Vikram Singh
+* Roll : 590042641
+* Day : 25 Question: 1
+* Date : 10-09-2026
+*
+* PROBLEM STATEMENT:
+* <Write a program to print the following pattern:
+5
+45
+345
+2345
+12345.>
+*/
+#include <stdio.h>
+int main() {
+    int i, j;
+
+    for (i = 5; i >= 1; i--) {
+    for (j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
